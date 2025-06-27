ab86de94ac165e5f69f584d48a431d3ae5193e4c
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-06-27 23:00:32 +0600

    Time: 0 ms (100.00%) | Memory: 23.9 MB (17.84%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+        void moveZeroes(vector<int>& nums) {
+            int n = nums.size(), j=0;
+            for(int i=0; i<n; i++){
+                if(nums[i]!=0){
+                    nums[j]=nums[i];
+                    j++;
+                } else nums[j]=nums[i];
+            }
+            while(j<n){
+                nums[j]=0;
+                j++;
+            }
+        }
+    };
