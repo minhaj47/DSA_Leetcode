fce481c376e703bf6b170cc9d21f36db84b20c08
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-09-29 18:43:31 +0600

    Time: 0 ms (100.00%) | Memory: 11.7 MB (49.23%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,12 +1,20 @@
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        unordered_map<int,int>mp;
+
+        for(auto x: nums){
+            mp[x]++;
+        }
+
         int n = nums.size();
 
-        for(int i=0; i<n-1; i++){
-            for(int j=0; j<n-i-1; j++){
-                if(nums[j]>nums[j+1])swap(nums[j], nums[j+1]);
+        for(int j=0, i=0; i<n; j++){
+            while(mp[j]--){
+                nums[i++]=j;
             }
         }
+        
     }
 };
