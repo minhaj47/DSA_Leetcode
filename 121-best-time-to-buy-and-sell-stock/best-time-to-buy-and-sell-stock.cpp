a910f4ca5301363a1a7301fea2ff0d22b32fc707
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-10-28 19:05:31 +0600

    Time: 0 ms (100.00%) | Memory: 97.4 MB (25.59%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -5,9 +5,6 @@ public:
         int mxInd = n-1, mnInd = n-1;
         for(int i = n-1; i>=0; i--){
             int prof = prices[mxInd] - prices[i];
-            // if(prof > mx){
-            //     m
-            // }
             mx = max(prof, mx);
             if(prof < 0)mxInd = i;
         }
