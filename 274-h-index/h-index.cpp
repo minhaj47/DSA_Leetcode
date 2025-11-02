4cdbfa480ee1776d1995bedd56b4bbb2aee674d6
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-11-02 12:59:56 +0600

    Time: 0 ms (100.00%) | Memory: 13 MB (11.38%) - LeetSync

--- 274-h-index/h-index.cpp
+++ 274-h-index/h-index.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int n = citations.size();
-        vector<int>count(n+1, 0);   
-        // count array relates to citation counts, not paper indices.
+        int totalPapers = citations.size();
+        vector<int> citationCount(totalPapers + 1, 0);
 
-        for(auto x: citations){
-            if(x>n){
-                count[n]++;
-            } else count[x]++;
+        for (int c : citations) {
+            if (c >= totalPapers)
+                citationCount[totalPapers]++;
+            else
+                citationCount[c]++;
         }
 
         int total = 0;
-
-        for(int i = n; i>=0; i--){
-            total+=count[i];
-            if(total >= i){
+        // Traverse from high citation counts to low
+        for (int i = totalPapers; i >= 0; i--) {
+            total += citationCount[i];
+            if (total >= i)
                 return i;
-            }
         }
+
         return 0;
     }
 };
