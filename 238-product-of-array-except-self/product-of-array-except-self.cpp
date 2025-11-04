e5f12c8665d67382137cc23dd9e15963b89c9343
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-11-04 11:53:50 +0600

    Time: 836 ms (5.94%) | Memory: 42.2 MB (25.68%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int product = 1;
-        int isZero = 0;
-        for(auto x: nums){
-            if(x==0){
-                isZero++;
-                continue;
-            }
-            product*=x;
-        }
         int n = nums.size();
+        vector<int>prefix(n);
+        prefix[0] = 1;
+        cout << prefix[0] << " ";
+        for(int i=1; i<n; i++){   
+            // here ith value will store the prefix till i-1 element
+            // as we needs till i-1 for ith result
+            prefix[i] = prefix[i-1] * nums[i-1];
+            cout << prefix[i] << " ";
+        } cout << endl;
+        vector<int>suffix(n);
+        suffix[n-1] = 1;
+        cout << suffix[n-1] << " ";
+        for(int i=n-2; i>=0; i--){
+            // here ith value will store the suffix from i+1 element
+            // as we needs from i+1 to n-1 for ith result
+            suffix[i] = suffix[i+1] * nums[i+1];
+            cout << suffix[i] << " ";
+        } cout << endl;
+
+        vector<int>ans(n);
+
         for(int i=0; i<n; i++){
-            if(isZero == 1 and nums[i]==0) {
-                nums[i] = product;
-            } else if(isZero) nums[i] = 0;
-            else nums[i] = product/nums[i];
+            ans[i] = suffix[i]*prefix[i];
         }
-        return nums;
+
+        return ans;
+
     }
 };
