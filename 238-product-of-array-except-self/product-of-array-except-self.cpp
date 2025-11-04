c42eabd613b0e6006a3a03df311201bb06ab1256
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-11-04 12:10:29 +0600

    Time: 855 ms (5.94%) | Memory: 40.2 MB (79.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int>prefix(n);
-        prefix[0] = 1;
-        cout << prefix[0] << " ";
+        vector<int>ans(n);
+        ans[0] = 1;
+        cout << ans[0] << " ";
         for(int i=1; i<n; i++){   
             // here ith value will store the prefix till i-1 element
             // as we needs till i-1 for ith result
-            prefix[i] = prefix[i-1] * nums[i-1];
-            cout << prefix[i] << " ";
+            ans[i] = ans[i-1] * nums[i-1];
+            cout << ans[i] << " ";
         } cout << endl;
-        vector<int>suffix(n);
-        suffix[n-1] = 1;
-        cout << suffix[n-1] << " ";
+        int suffix_pro = 1;
+        cout << suffix_pro << " ";
         for(int i=n-2; i>=0; i--){
-            // here ith value will store the suffix from i+1 element
+            // here suffix_pro value will carry the suffix from i+1th element
             // as we needs from i+1 to n-1 for ith result
-            suffix[i] = suffix[i+1] * nums[i+1];
-            cout << suffix[i] << " ";
+            suffix_pro = nums[i+1] * suffix_pro;
+            ans[i] = ans[i] * suffix_pro;
+            cout << suffix_pro << " ";
         } cout << endl;
-
-        vector<int>ans(n);
-
-        for(int i=0; i<n; i++){
-            ans[i] = suffix[i]*prefix[i];
-        }
-
         return ans;
-
     }
 };
