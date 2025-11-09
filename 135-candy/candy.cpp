ac4393c8d19c2d6866f09151e67fa9ba03a12cb6
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-11-09 12:28:16 +0600

    Time: 210 ms (5.05%) | Memory: 21.9 MB (97.58%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        // consider left and right neighbour separately 
-        // remember higher ratings needs higher no of candy 
-        // not necessarily lower should get lower. 
-        // As came across left and right lower also get lower by the end. 
-
         int n = ratings.size();
-        vector<int>left(n), right(n);
-        left[0] = 1, right[n-1] = 1;
 
-        for(int i=1; i<n; i++){
-            left[i] = (ratings[i] > ratings[i-1]) ? left[i-1] + 1 : 1;
+        int i = 1, candy = 1;
+
+        while(i<n){
+            if(ratings[i] == ratings[i-1]){
+                cout << 1 << " ";
+                candy++, i++;
+                continue;
+            }
+
+            int peak = 1;
+            while(i<n && ratings[i] > ratings[i-1]){
+                peak++, i++;
+                cout << peak << " ";
+                candy+=peak;
+            }
+
+            int down = 1;
+            while(i<n && ratings[i] < ratings[i-1]){
+                cout << down << " ";
+                candy+=down;
+                i++, down++;
+            }
+
+            if(down>peak) candy += (down - peak);
         }
-
-        for(int i=n-2; i>=0; i--){
-            right[i] = (ratings[i] > ratings[i+1]) ? right[i+1] + 1 : 1;
-        }
-
-        int candy = 0;
-
-        for(int i=0; i<n; i++){
-            cout << left[i] << " " << right[i] << endl;
-            candy += max(left[i], right[i]);
-        }
-
         return candy;
     }
 };
