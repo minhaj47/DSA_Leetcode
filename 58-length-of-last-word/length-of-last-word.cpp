c9861d36b962094c6cfa68d07b2af639ef23e0bb
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-11-11 12:34:02 +0600

    Time: 0 ms (100.00%) | Memory: 8.8 MB (90.13%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.cpp
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -3,16 +3,9 @@ public:
     int lengthOfLastWord(string s) {
         int n = s.size(), l = 0;
         int i= n-1;
-        
-        while(s[i] == ' '){
-            i--;
-        }
 
-        while(i>=0){
-            if(s[i] == ' ') break;
-            l++;
-            i--;
-        }
+        while(i>=0 && s[i] == ' ') i--;
+        while(i>=0 && s[i] != ' ') l++, i--;
         return l;
     }
 };
