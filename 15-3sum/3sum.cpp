57ae83e62e15d44ca1a21974389e49c5094c714d
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-11-17 15:46:46 +0600

    Time: 57 ms (28.48%) | Memory: 29 MB (90.23%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -11,8 +11,7 @@ public:
             while(j<k){
                 int sum = nums[i] + nums[j] + nums[k];
                 if(sum == 0){
-                    vector<int> tmp = {nums[i], nums[j], nums[k]};
-                    ans.push_back(tmp);
+                    ans.push_back({nums[i], nums[j], nums[k]});
                     while(j<k && nums[j] == nums[j+1])j++;
                     while(j<k && nums[k] == nums[k-1])k--;
                     j++, k--;
