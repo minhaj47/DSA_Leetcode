d7aac9834d7b37031957e3c97dfda3eff84d7e99
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-11-22 15:44:45 +0600

    Time: 8 ms (19.04%) | Memory: 11.8 MB (48.87%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int>mp;
+        for(auto c: magazine){
+            mp[c]++;
+        }
+        for(auto c: ransomNote) {
+            if(mp[c]<1) return false;
+            mp[c]--;
+        }
+        return true;
+    }
+};
