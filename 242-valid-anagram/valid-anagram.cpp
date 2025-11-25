3ac7ce58ea62a922ee4f1cff76f4dc335c3ab342
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-11-25 18:05:33 +0600

    Time: 0 ms (100.00%) | Memory: 9.9 MB (25.37%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        unordered_map<char, int>um;
+        int n = s.size(), m = t.size();
+        if(n!=m) return false;
 
-        if(s == t) return true;
-        return false;
+        for(int i=0; i<n; i++){
+            um[s[i]]++;
+            um[t[i]]--;
+        }
+        for(auto [key, value]: um) {
+            if(value != 0)return false;
+        }
+        return true;
     }
 };
