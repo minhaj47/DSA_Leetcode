d28e392cd478e42aeaf522d87263f6726ee08901
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-12-06 01:28:52 +0600

    Time: 0 ms (100.00%) | Memory: 8.8 MB (87.34%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char x: s) {
+            if(x == '(' or x == '{' or x == '[') {
+                st.push(x);
+            } else {
+                if(st.empty()) return false;
+                char c = st.top();
+                st.pop();
+                if((x ==')' and c !='(') || 
+                (x == '}' and c != '{') || 
+                (x == ']' and c != '[')) return false;
+            }
+        }
+        return st.empty();
+    }
+};
