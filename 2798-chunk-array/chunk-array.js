0a567e2dd91be48dc14ce2946d9f16c216f2dc04
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2025-12-31 11:59:20 +0600

    Time: 41 ms (89.21%) | Memory: 56.3 MB (47.32%) - LeetSync

--- 2798-chunk-array/chunk-array.js
+++ 2798-chunk-array/chunk-array.js
@@ -7,14 +7,19 @@ var chunk = function(arr, size) {
     // return _.chunk(arr, size);
 
     const result = [];
-    let temp = [];
-    for(let i=0; i<arr.length; i++) {
-        if(i%size == 0 && i!=0) {
-            result.push(temp);
-            temp = [];
-        }
-        temp.push(arr[i]);
+
+    // let temp = [];
+    // for(let i=0; i<arr.length; i++) {
+    //     if(i%size == 0 && i!=0) {
+    //         result.push(temp);
+    //         temp = [];
+    //     }
+    //     temp.push(arr[i]);
+    // }
+    // if(temp.length)result.push(temp);
+
+    for(let i = 0; i < arr.length; i+=size) {
+        result.push(arr.slice(i, i + size));
     }
-    if(temp.length)result.push(temp);
     return result;
 };
