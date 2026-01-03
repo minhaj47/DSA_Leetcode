df969547cc5128e3d41bebae2ff498d092f388a4
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2026-01-03 21:39:10 +0600

    Time: 0 ms (100.00%) | Memory: 18.7 MB (5.43%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -10,18 +10,22 @@
  * };
  */
 class Solution {
-private: 
-    bool symmetricTraversal(TreeNode* l1, TreeNode* l2) {
-        if(!l1 && !l2) return true;
-        if(!l1 || !l2 || l1->val != l2->val) return false;
-        
-        return symmetricTraversal(l1->left, l2->right) && 
-            symmetricTraversal(l1->right, l2->left);
-    }
 public:
     bool isSymmetric(TreeNode* root) {
         if(!root) return true;
         
-        return symmetricTraversal(root->left, root->right);
+        queue<pair<TreeNode*, TreeNode*>>q;
+        q.push({root->left, root->right});
+
+        while(!q.empty()) {
+            auto [a, b] = q.front();
+            q.pop();
+            if(!a and !b) continue;
+            if(!a or !b or a->val != b->val) return false;
+
+            q.push({a->left, b->right});
+            q.push({a->right, b->left});
+        }
+        return true;
     }
 };
