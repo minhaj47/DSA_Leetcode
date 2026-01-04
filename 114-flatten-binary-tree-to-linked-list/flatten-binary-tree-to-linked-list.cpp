8ce97bb82f4526fc4ca9bf17c1ab0a1dfc6dee8a
Md. Minhajul Haque <106464951+minhaj47@users.noreply.github.com>
2026-01-04 22:26:14 +0600

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.76%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -10,24 +10,38 @@
  * };
  */
 class Solution {
-private:
-    TreeNode* helper(TreeNode* root) {
-        if(!root) return nullptr;
+// private:
+//     TreeNode* helper(TreeNode* root) {
+//         if(!root) return nullptr;
 
-        TreeNode* leftTail = helper(root->left);
-        TreeNode* rightTail = helper(root->right);
+//         TreeNode* leftTail = helper(root->left);
+//         TreeNode* rightTail = helper(root->right);
 
-        if(root->left) {
-            TreeNode* temp = root->right;
-            root->right = root->left;
-            root->left = nullptr;
-            leftTail->right = temp;
-        }
+//         if(root->left) {
+//             TreeNode* temp = root->right;
+//             root->right = root->left;
+//             root->left = nullptr;
+//             leftTail->right = temp;
+//         }
 
-        return rightTail ? rightTail : (leftTail ? leftTail : root);
+//         return rightTail ? rightTail : (leftTail ? leftTail : root);
+//     }
+private: 
+    void preorder(TreeNode* root, vector<TreeNode*>& nodes) {
+        if(!root) return;
+        nodes.push_back(root);
+        preorder(root->left, nodes);
+        preorder(root->right, nodes);
     }
 public:
     void flatten(TreeNode* root) {
-        helper(root);
+        if(!root) return;
+        vector<TreeNode*> nodes; 
+        preorder(root, nodes);
+
+        for(int i=0; i<nodes.size() - 1; i++) {
+            nodes[i]->left = nullptr;
+            nodes[i]->right = nodes[i+1];
+        }
     }
 };
